60c731e637115e6e45723585cde9c3fc2a7ab751
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-19 04:11:31 -0300

    feat: add createAccessToken auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { sign } from 'jsonwebtoken';
+import { Model } from 'mongoose';
+import { User } from 'src/users/models/users.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel('User')
+    private readonly userModel: Model<User>,
+  ) {}
+
+  public async createAccessToken(userId: string): Promise<string> {
+    return sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRATION,
+    });
+  }
+}
