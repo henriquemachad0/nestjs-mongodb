f8007e5cfef1544ccbf8d19e398d55ff8b56e599
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-19 04:22:40 -0300

    feat: add jwtExtractor auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Request } from 'express';
 import { sign } from 'jsonwebtoken';
 import { Model } from 'mongoose';
 import { User } from 'src/users/models/users.model';
@@ -15,5 +20,27 @@ export class AuthService {
     return sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRATION,
     });
+  }
+
+  public async validateUser(userId: string): Promise<User> {
+    const user = await this.userModel.findOne({ _id: userId });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found.');
+    }
+
+    return user;
+  }
+
+  private jwtExtractor(request: Request): string {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new BadRequestException('Bad request.');
+    }
+
+    const [, token] = authHeader.split('');
+
+    return token;
   }
 }
