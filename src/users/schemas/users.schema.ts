4cdfbe3384ca908023e109c0e225a58f317939e3
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-19 03:57:39 -0300

    feat: commit

---
+++ src/users/schemas/users.schema.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+export const UsersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+});
+
+UsersSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    this['password'] = await bcrypt.hash(this['password'], 10);
+  } catch (error) {
+    next(error);
+  }
+});
