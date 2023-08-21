db5c7322d45288d2a4f3108b3fd47677afedd05f
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-20 21:48:11 -0300

    feat: add methods in users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignupDto } from './dto/signup.dto';
+import { User } from './models/users.model';
+import { SigninDto } from './dto/signin.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  public async signup(@Body() signupDto: SignupDto): Promise<User> {
+    const teste = await this.usersService.signup(signupDto);
+    console.log('CONSOLE: ', teste);
+    return teste;
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  public async signin(
+    @Body() signinDto: SigninDto,
+  ): Promise<{ name: string; jwtToken: string; email: string }> {
+    return this.usersService.signin(signinDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public async findAll(): Promise<User[]> {
+    return await this.usersService.findAll();
+  }
+}
