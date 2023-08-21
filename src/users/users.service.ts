6d948439f17fa6157fa4bd6799a6846b71b9604f
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-20 21:10:33 -0300

    feat: add methods in users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { Model } from 'mongoose';
+import { User } from './models/users.model';
+import { AuthService } from 'src/auth/auth.service';
+import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel('User')
+    private readonly userModel: Model<User>,
+    private readonly authService: AuthService,
+  ) {}
+
+  public async signup(signupDto: SignupDto): Promise<User> {
+    const user = new this.userModel(signupDto);
+    return user.save();
+  }
+
+  public async signin(
+    signinDto: SigninDto,
+  ): Promise<{ name: string; jwtToken: string; email: string }> {
+    const user = await this.findByEmail(signinDto.email);
+    const match = await this.checkPassword(signinDto.password, user);
+
+    if (!match) {
+      throw new NotFoundException('Invalid credentials');
+    }
+
+    const jwtToken = await this.authService.createAccessToken(user._id);
+
+    return {
+      name: user.name,
+      jwtToken,
+      email: user.email,
+    };
+  }
+
+  public async findAll(): Promise<User[]> {
+    return await this.userModel.find();
+  }
+
+  private async findByEmail(email: string): Promise<User> {
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new NotFoundException('Email not found.');
+    }
+
+    return user;
+  }
+
+  private async checkPassword(password: string, user: User): Promise<boolean> {
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      throw new NotFoundException('Password not found.');
+    }
+
+    return match;
+  }
+}
